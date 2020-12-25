974bd74d10a66d4dd96eb4473e82852b979fe038
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-25 12:41:38 +0900

    タグのハッシュタグ化

--- backend/app/Models/Tag.php
+++ backend/app/Models/Tag.php
@@ -11,4 +11,9 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function getHashtagAttribute(): string
+    {
+        return '#' . $this->name;
+    }
 }
