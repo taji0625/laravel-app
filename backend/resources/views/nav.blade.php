f65985f6e5a8d863fb2cbd8e8f73d94e3b598bcb
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-25 18:14:55 +0900

    followsテーブルの作成

--- backend/resources/views/nav.blade.php
+++ backend/resources/views/nav.blade.php
@@ -24,8 +24,7 @@
     @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-user-circle"></i>
       </a>
       <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
