79381e3d49a6cf27f4d597232edff9c7e41efc60
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-25 17:36:52 +0900

    ユーザー詳細ページの作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,4 +30,9 @@ Route::prefix('articles')->name('articles.')->group(function () {
     Route::put('/{article}/like', [ArticleController::class, 'like'])->name('like')->middleware('auth');
     Route::delete('/{article}/like', [ArticleController::class, 'unlike'])->name('unlike')->middleware('auth');
 });
+
 Route::get('/tags/{name}', [TagController::class, 'show'])->name('tags.show');
+
+Route::prefix('users')->name('users.')->group(function(){
+    Route::get('/{name}', [UserController::class, 'show'])->name('show');
+});
